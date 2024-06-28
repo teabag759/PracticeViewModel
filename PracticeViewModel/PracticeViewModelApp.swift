9bd6a1559f8f426474e933a130904865190f827d
Sohyeon Lee <teabag759@gmail.com>
2024-06-28 15:08:23 +0900

    Initial Commit

---
+++ PracticeViewModel/PracticeViewModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeViewModelApp.swift
+//  PracticeViewModel
+//
+//  Created by 이소현 on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeViewModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
